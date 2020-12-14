04efc74770b253da8ea9e8e560015fdcea32e8d1
pengx17 <pengxiao@outlook.com>
2020-12-14 15:40:34 +0800

    chore: demo test

--- demo/parent/index.tsx
+++ demo/parent/index.tsx
@@ -1,4 +1,6 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
+import * as React from '../web_modules/react.js';
+import * as ReactDOM from '../web_modules/react-dom.js';
+
+import {} from '.';
 
 ReactDOM.render(<h1>Hello world</h1>, document.getElementById('app'));
